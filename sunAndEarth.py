92a44149721c61f8d60c3fd6a9fdea40fb6a9e9f
Jan-Willem Goossens <jan.willem.goossens@gmail.com>
2020-04-10 17:07:27 +0200

    cleanup

--- sunAndEarth.py
+++ sunAndEarth.py
@@ -1,40 +1,53 @@
 import matplotlib.pyplot as plt
 from vector3 import Vector3
 from physics import Gravity, Entity
+import itertools
 
-
-
+ents = []
+# Initialize sun
 pos_sun = Vector3(0, 0, 0)
 v0_sun = Vector3(0, 0, 0)
 m_sun = 1.98847e30
-sun   = Entity(pos_sun, v0_sun, m_sun, [])
-
-pos_earth = Vector3(1.4959787e11, 0,0)
+sun = Entity(pos_sun, v0_sun, m_sun, [])
+ents.append(sun)
+# Initialize earth
+pos_earth = Vector3(1.4959787e11, 0, 0)
 v0_earth = Vector3(0, 29.78e3, 0)
 m_earth = 5.9722e24
 earth = Entity(pos_earth, v0_earth, m_earth, [])
+ents.append(earth)
 
-sun.forces   = [Gravity(earth, sun)]
-earth.forces = [Gravity(sun, earth)]
+##Add gravity between all entities
+for o1, o2 in itertools.permutations(ents):
+    o1.forces = [Gravity(o2, o1)]
 
+# Time steps of 1/1000th of a day (in seconds)
+dt = 0.001 * 365 * 24 * 60 * 60
+N_steps = 990
 
-dt = 0.001*365*24*60*60
-Nsteps =990
-
-ts = [dt*i for i in range( Nsteps)]
-poslog = []
-vlog   = []
+# Time array
+ts = [dt * i for i in range(N_steps)]
+p_log = []
+v_log = []
 for _ in ts:
-    earth.update(dt)
-    sun.update(dt)
-    poslog.append(earth.pos)
-    vlog.append(earth.velocity)
-xs = [p.x for p in poslog]
-ys = [p.y for p in poslog]
-zs = [p.z for p in poslog]
-vxs = [v.x for v in vlog]
-vys = [v.y for v in vlog]
-vzs = [v.z for v in vlog]
+    for o in ents:
+        o.update(dt)  # update step for every physical entity
+    p_log.append(earth.pos)
+    v_log.append(earth.velocity)
 
-plt.plot(xs,ys)
+
+
+
+
+
+
+
+xs = [p.x for p in p_log]
+ys = [p.y for p in p_log]
+zs = [p.z for p in p_log]
+vxs = [v.x for v in v_log]
+vys = [v.y for v in v_log]
+vzs = [v.z for v in v_log]
+
+plt.plot(xs, ys)
 plt.show()
